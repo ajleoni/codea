fda620b4462a5650bb31495702f56f56e52a62a4
Alessandro J. Leoni <1433131+ajleoni@users.noreply.github.com>
2021-09-11 10:07:16 +0200

    added some documentation to the code

--- mixedMessages/start.js
+++ mixedMessages/start.js
@@ -5,13 +5,16 @@ let subject =['Far','Mor','Noemi','Vanessa','Lenu','Benji'];
 let verbum =['spiser','kaster','husker','angriber','læser','studerer','hjælper'];
 let predicate=['æbler','sten','snegle','bogen','computer','matematik','skraldet'];
 
+// function to select a random word from an array
 const selectWord = (arr) => {
     let i = Math.floor(Math.random()*arr.length);
     return arr[i];
 }
 
+// probability that the subject word is also the predicate
 let p = 0.3;
 
+// Code to output the full message
 if (p>Math.random()) {
     console.log(`My Message is: ${selectWord(subject)} ${selectWord(verbum)} ${selectWord(predicate)}!`);
 } else {
